d284046fc82436316e9960e6796efa5d85f31109
roshan-jha-23 <jha062305@gmail.com>
2024-06-05 07:27:34 +0530

    work done

--- coderstop/src/app/dashboard/page.tsx
+++ coderstop/src/app/dashboard/page.tsx
@@ -6,7 +6,7 @@ interface UserData {
   name: string;
   email: string;
   username: string;
-  skill: string;
+  skills: string;
   favoriteCodingLanguage: string;
   bio: string;
   profilePicUrl: string;
@@ -50,38 +50,40 @@ function Dashboard() {
   }
 
   return (
-    <div className="relative h-screen p-8">
+    <div className="relative h-screen p-8 bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 text-white">
       <div className="absolute top-0 right-0 m-4">
         <Logout />
       </div>
-      <div className="max-w-4xl mx-auto mt-10 bg-white p-6 rounded-lg shadow-md">
+      <div className="max-w-4xl mx-auto mt-24 bg-white bg-opacity-80 p-6 rounded-lg shadow-md">
         <div className="flex items-center space-x-4">
           <img
             src={userData.profilePicUrl}
             alt="Profile"
-            className="w-20 h-20 rounded-full"
+            className="w-20 h-20 rounded-full border-4 border-blue-500"
           />
           <div>
-            <h2 className="text-2xl font-bold">{userData.name}</h2>
+            <h2 className="text-2xl font-bold text-black">{userData.name}</h2>
             <p className="text-gray-600">@{userData.username}</p>
           </div>
         </div>
         <div className="mt-4">
-          <h3 className="text-xl font-semibold">About Me</h3>
+          <h3 className="text-xl font-semibold text-black">About Me</h3>
           <p className="mt-2 text-gray-700">{userData.bio}</p>
         </div>
         <div className="mt-4">
-          <h3 className="text-xl font-semibold">Skills</h3>
-          <p className="mt-2 text-gray-700">{userData.skill}</p>
+          <h3 className="text-xl font-semibold text-black">Skills</h3>
+          <p className="mt-2 text-gray-700">{userData.skills}</p>
         </div>
         <div className="mt-4">
-          <h3 className="text-xl font-semibold">Favorite Coding Language</h3>
+          <h3 className="text-xl font-semibold text-black">
+            Favorite Coding Language
+          </h3>
           <p className="mt-2 text-gray-700">
             {userData.favoriteCodingLanguage}
           </p>
         </div>
         <div className="mt-4">
-          <h3 className="text-xl font-semibold">Contact</h3>
+          <h3 className="text-xl font-semibold text-black">Contact</h3>
           <p className="mt-2 text-gray-700">{userData.email}</p>
         </div>
       </div>
